995c043755cc0322e763dca278f51008e382468d
Makrand <makrandshingare@gmail.com>
2022-01-23 19:24:33 +0530

    Added : Created Address book and print data

---
+++ AddressBookModel.js
@@ -0,0 +1,24 @@
+class AddressBook{
+    //property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    constructor(firstName,lastName,address,city,state,zip,phone,email)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=address;
+        this.state=state;
+        this.zip=zip;
+        this.phone=phone;
+        this.email=email;
+    }
+
+}
--- AddressBookSystem.js
+++ AddressBookSystem.js
@@ -1,1 +1,41 @@
 console.log("Welcome in Address book");
+class AddressBook{
+    //property
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    constructor(firstName,lastName,address,city,state,zip,phone,email)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=address;
+        this.state=state;
+        this.zip=zip;
+        this.phone=phone;
+        this.email=email;
+    }
+
+}
+var prompt = require('prompt-sync')();
+//let addressBook=new AddressBookModel();
+function getData(){
+    let readline =require('prompt-sync')();
+    let firstName=readline("Enter the First Name : ");
+    let lastName=readline("Enter the Last Name : ");
+    let address=readline("Enter the address : ");
+    let city=readline("Enter the city Name : ");
+    let state=readline("Enter the state Name : ");
+    let zip=readline("Enter the zip Code : ");
+    let phone=readline("Enter the phone Number : ");
+    let email=readline("Enter the Email id : ");
+    return(new AddressBook(firstName,lastName,address,city,state,zip,phone,email));
+}
+
+console.log(getData);
